d9feb0a00df469635beb5c1603ad9a9e5e37d540
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-11-29 17:21:06 +0100

    Create login route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const { validationResult } = require('express-validator');
 const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 
 const User = require('../models/User');
 const userValidators = require('../validation/user');
+const userLoginValidators = require('../validation/userLogin');
 const reportError = require('../functions/reportError');
 
 router.post('/create', userValidators, async (req, res) => {
@@ -57,8 +59,46 @@ router.post('/create', userValidators, async (req, res) => {
   });
 });
 
-router.post('/login', async (req, res) => {
-  res.send('User login dummy route');
+router.post('/login', userLoginValidators, async (req, res) => {
+  // Validate user data
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array()
+    });
+  }
+
+  // Check if the user exists
+  let user;
+  try {
+    user = await User.findOne({ username: req.body.username });
+  } catch (e) {
+    reportError(e);
+    return res.status(500).send();
+  }
+  if (!user) {
+    return res.status(404).send();
+  }
+
+  // check if the password matches
+  try {
+    if (!(await argon2.verify(user.password, req.body.password))) {
+      return res.status(400).json({ msg: 'Invalid password' });
+    }
+  } catch (e) {
+    reportError(e);
+    return res.status(500).send();
+  }
+
+  // everything is good, send the JWT
+  const payload = {
+    id: user._id,
+    username: user.username
+  };
+  const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    expiresIn: '6h'
+  });
+  res.json({ token });
 });
 
 module.exports = router;
