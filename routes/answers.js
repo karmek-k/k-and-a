b6876494e5715663f71948cdcbfe92b89a180be1
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-11-30 17:54:34 +0100

    Create answer upvote route

--- routes/answers.js
+++ routes/answers.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
 
 const auth = require('./middleware/authMiddleware');
 const disallowBanned = require('./middleware/disallowBanned');
@@ -7,6 +8,8 @@ const answerCreateValidators = require('../validation/answerCreate');
 
 const Answer = require('../models/Answer');
 const Question = require('../models/Question');
+
+const reportError = require('../functions/reportError');
 
 router.post(
   '/create',
@@ -46,5 +49,45 @@ router.post(
     return res.status(201).json(answer);
   }
 );
+
+router.post('/upvote/:id', auth, disallowBanned, async (req, res) => {
+  const upvoterId = req.user.id;
+
+  // Check if the id is correct
+  let answer;
+  try {
+    answer = await Answer.findById(req.params.id);
+  } catch (e) {
+    return res.status(400).json({ msg: 'Invalid question id' });
+  }
+
+  // Check if the answer exists
+  if (!answer) {
+    return res.status(404).json({ msg: 'Answer not found' });
+  }
+
+  // Check if the user has already upvoted this answer
+  const upvoters = answer.upvotersIds;
+  if (upvoters.find(id => id == upvoterId)) {
+    return res
+      .status(403)
+      .json({ msg: 'You have already upvoted this answer' });
+  }
+
+  // Check if the user is upvoting themselves
+  if (answer.posterId === upvoterId) {
+    return res.status(400).json({ msg: "You can't upvote your own answer" });
+  }
+
+  upvoters.push(new mongoose.Types.ObjectId(upvoterId));
+  try {
+    await answer.updateOne({ upvotersIds: upvoters });
+  } catch (e) {
+    reportError(e);
+    return res.status(500).send();
+  }
+
+  return res.json({ answer });
+});
 
 module.exports = router;
