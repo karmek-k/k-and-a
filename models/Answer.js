ceaebb5a1f1758770afdd00dc89309d9c37c01fc
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-11-30 15:07:23 +0100

    Create Answer model

---
+++ models/Answer.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const answerSchema = new mongoose.Schema({
+  posterId: {
+    type: mongoose.Types.ObjectId,
+    required: true
+  },
+  questionId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    index: { unique: true }
+  },
+  content: {
+    type: String,
+    required: True
+  },
+  votes: {
+    type: Number,
+    default: 0
+  }
+});
+
+module.exports = mongoose.model('Answer', answerSchema);
